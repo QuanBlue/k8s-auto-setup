9d63399e31c183ac93b8ad4167c52f0bf71eb68e
Nguyen Thanh Quan <quannguyenthanh558@gmail.com>
2024-01-09 10:29:11 +0700

    fix: solve WARNING: apt does not have a stable CLI interface. Use with caution in scripts.

--- install-docker-kube.sh
+++ install-docker-kube.sh
@@ -16,7 +16,7 @@ echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://a
 sudo apt-get update && sudo apt-get install -y kubelet=1.26.5-00 kubeadm=1.26.5-00 kubectl=1.26.5-00
 
 # install Docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
