4fd66d5f1e49d9ab1c58e87115a80a62d852ebfd
Nguyen Thanh Quan <quannguyenthanh558@gmail.com>
2024-01-10 10:08:08 +0700

    fix: error all nodes have same Internal IP
    add Environment in kubeadm conf - ref:https://stackoverflow.com/questions/51154911/kubectl-exec-results-in-error-unable-to-upgrade-connection-pod-does-not-exi

--- install-docker-kube.sh
+++ install-docker-kube.sh
@@ -17,9 +17,9 @@ echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://a
 sudo apt-get update -y && sudo apt-get install -y kubelet=1.26.5-00 kubeadm=1.26.5-00 kubectl=1.26.5-00
 
 # install Docker
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt-get update -y && sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 apt-cache policy docker-ce
 sudo apt-get install -y docker-ce
 sudo usermod -aG docker ${USER}
